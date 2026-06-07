b50c2e7da9d70e38db743f319863d2ff6730dbd1
Gaetano-Leanza <gaetano1981@live.de>
2026-06-07 21:22:55 +0200

    work on serializers.py

--- config/serializers.py
+++ config/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from .models import Profile
+
 
 class RegistrationSerializer(serializers.ModelSerializer):
     repeated_password = serializers.CharField(write_only=True)
-    type = serializers.CharField(write_only=True)  
+    type = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
@@ -15,24 +17,51 @@ class RegistrationSerializer(serializers.ModelSerializer):
 
     def validate(self, data):
         if data['password'] != data['repeated_password']:
-            raise serializers.ValidationError({"password": "Passwörter stimmen nicht überein."})
+            raise serializers.ValidationError(
+                {"password": "Passwörter stimmen nicht überein."})
         if User.objects.filter(email=data['email']).exists():
-            raise serializers.ValidationError({"email": "Ein Benutzer mit dieser E-Mail existiert bereits."})
+            raise serializers.ValidationError(
+                {"email": "Ein Benutzer mit dieser E-Mail existiert bereits."})
         return data
 
     def create(self, validated_data):
         validated_data.pop('repeated_password')
+        user_type = validated_data.pop('type')
 
-        user_type = validated_data.pop('type') 
-        
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password']
         )
+
+        Profile.objects.create(
+            user=user,
+            type=user_type
+        )
+
         return user
 
 
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField(write_only=True)
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(source='user.username', read_only=True)
+    email = serializers.CharField(source='user.email', read_only=True)
+
+    class Meta:
+        model = Profile
+        fields = [
+            'user', 'username', 'first_name', 'last_name', 'file',
+            'location', 'tel', 'description', 'working_hours',
+            'type', 'email', 'created_at'
+        ]
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        for key, value in data.items():
+            if value is None:
+                data[key] = ""
+        return data
