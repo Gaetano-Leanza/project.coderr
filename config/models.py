4b46873ba1959dec5a546fbdca433f50410553dd
Gaetano-Leanza <gaetano1981@live.de>
2026-06-07 21:15:46 +0200

    add models.py

---
+++ config/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=150, blank=True, default="")
+    last_name = models.CharField(max_length=150, blank=True, default="")
+    file = models.FileField(upload_to='profile_pictures/', blank=True, null=True)
+    location = models.CharField(max_length=100, blank=True, default="")
+    tel = models.CharField(max_length=30, blank=True, default="")
+    description = models.TextField(blank=True, default="")
+    working_hours = models.CharField(max_length=100, blank=True, default="")
+    type = models.CharField(max_length=20, default="customer")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Profil von {self.user.username}"
