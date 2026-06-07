254fe48a82de3817a2dd97320f34018468b085f0
Gaetano-Leanza <gaetano1981@live.de>
2026-06-07 21:34:31 +0200

    add api folder

--- api/models.py
+++ api/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=150, blank=True, default="")
     last_name = models.CharField(max_length=150, blank=True, default="")
-    file = models.FileField(upload_to='profile_pictures/', blank=True, null=True)
+    file = models.FileField(
+        upload_to='profile_pictures/', blank=True, null=True)
     location = models.CharField(max_length=100, blank=True, default="")
     tel = models.CharField(max_length=30, blank=True, default="")
     description = models.TextField(blank=True, default="")
