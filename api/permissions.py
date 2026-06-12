4a629954e7895803349eae2a8ed30e515c104c3b
Gaetano-Leanza <gaetano1981@live.de>
2026-06-12 22:30:52 +0200

    add IsBusinessProfile to permissions.py

--- api/permissions.py
+++ api/permissions.py
@@ -6,3 +6,14 @@ class IsOwnerProfile(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
 
         return obj.user == request.user
+
+
+class IsBusinessProfile(permissions.BasePermission):
+    message = "Nur User vom type 'business' dürfen Angebote erstellen."
+
+    def has_permission(self, request, view):
+
+        if not request.user.is_authenticated:
+            return False
+
+        return hasattr(request.user, 'profile') and request.user.profile.type == 'business'
