5dd63d0305028f875a0d1be745dd6733cc8c5cb0
Gaetano-Leanza <gaetano1981@live.de>
2026-06-15 22:58:41 +0200

    update admin.py

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Profile, Offer, OfferDetail
+from .models import Profile, Offer, OfferDetail, Order
 
 
 admin.site.register(Profile)
 admin.site.register(Offer)
 admin.site.register(OfferDetail)
+admin.site.register(Order)
