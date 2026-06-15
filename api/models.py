54995132b88b948baae0fb6a778e89f5968b1ed5
Gaetano-Leanza <gaetano1981@live.de>
2026-06-15 22:40:50 +0200

    add Order to models.py

--- api/models.py
+++ api/models.py
@@ -19,18 +19,17 @@ class Profile(models.Model):
         return f"Profil von {self.user.username}"
 
 
-
-
 class Offer(models.Model):
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     image = models.FileField(upload_to='offer_images/', blank=True, null=True)
     description = models.TextField()
-    
-    min_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+
+    min_price = models.DecimalField(
+        max_digits=10, decimal_places=2, default=0.00)
     min_delivery_time = models.IntegerField(default=1)
-    
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -39,14 +38,43 @@ class Offer(models.Model):
 
 
 class OfferDetail(models.Model):
-    offer = models.ForeignKey(Offer, on_delete=models.CASCADE, related_name='details')
+    offer = models.ForeignKey(
+        Offer, on_delete=models.CASCADE, related_name='details')
     title = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     delivery_time_in_days = models.IntegerField()
     description = models.TextField(blank=True, default="")
     revisions = models.IntegerField(default=0)
-    features = models.JSONField(default=list) 
+    features = models.JSONField(default=list)
     offer_type = models.CharField(max_length=20, default='basic')
 
     def __str__(self):
         return f"Detail für {self.offer.title}"
+
+
+class Order(models.Model):
+    STATUS_CHOICES = [
+        ('in_progress', 'In Bearbeitung'),
+        ('completed', 'Abgeschlossen'),
+        ('cancelled', 'Storniert'),
+    ]
+
+    customer_user = models.ForeignKey(
+        User, related_name='customer_orders', on_delete=models.CASCADE)
+    business_user = models.ForeignKey(
+        User, related_name='business_orders', on_delete=models.CASCADE)
+
+    title = models.CharField(max_length=200)
+    revisions = models.IntegerField(default=0)
+    delivery_time_in_days = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    features = models.JSONField(default=list)
+    offer_type = models.CharField(max_length=20)
+    status = models.CharField(
+        max_length=20, choices=STATUS_CHOICES, default='in_progress')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Order #{self.id} - {self.title}"
