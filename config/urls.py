4cbb2fab8a62624eb6a94d20cd0423891a89c9d0
Gaetano-Leanza <gaetano1981@live.de>
2026-06-15 22:55:46 +0200

    add url to urls.py

--- config/urls.py
+++ config/urls.py
@@ -23,7 +23,7 @@ from api.views import (
     LoginView,
     ProfileDetailView,
     OfferListView,
-    OfferDetailView, SingleOfferDetailView
+    OfferDetailView, SingleOfferDetailView, OrderListView
 )
 
 urlpatterns = [
@@ -39,4 +39,5 @@ urlpatterns = [
     path('api/offers/<int:pk>/', OfferDetailView.as_view(), name='offer-detail'),
     path('api/offerdetails/<int:pk>/',
          SingleOfferDetailView.as_view(), name='offerdetail-detail'),
+    path('api/orders/', OrderListView.as_view(), name='order-list'),
 ]
