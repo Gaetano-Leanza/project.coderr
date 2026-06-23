8a4e46b38261a9a014b0e8031d1c39c8a291cce7
Gaetano-Leanza <gaetano1981@live.de>
2026-06-23 22:44:28 +0200

    add Review to models.py

--- api/models.py
+++ api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Profile(models.Model):
@@ -78,3 +79,18 @@ class Order(models.Model):
 
     def __str__(self):
         return f"Order #{self.id} - {self.title}"
+
+
+class Review(models.Model):
+    business_user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='business_reviews')
+    reviewer = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='reviews_given')
+    rating = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)])
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Review by {self.reviewer.username} for {self.business_user.username}"
